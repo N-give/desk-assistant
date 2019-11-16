163531a16f5e3f9e0fa666c447e83d4f6fbed532
Nathan Givens <nate.givens03@gmail.com>
2019-11-16 17:05:40 -0500

    feat: draw box w/rounded corners to display event
    NOTE: This only works for events that that fall directly on a time line
    that is drawn (because of how the position for the box lines is found).
    In the future, the positions should be found based on those lines that
    it falls between and its position calculated based on the width between
    those lines. Currently considering calculating its percentage of the way
    to the next line and then moving that far to the next line.

--- draw_screen.py
+++ draw_screen.py
@@ -1,29 +1,21 @@
 '''Draw class implementation for e-paper desk assisstant'''
-
-# import sys picdir =
-# os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
-# 'pic') libdir =
-# os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
-# 'lib') if os.path.exists(libdir): sys.path.append(libdir)
-
-import os
-import time
+from typing import Tuple, Dict, List
+# import os
+# import time
 from datetime import datetime
-from typing import Tuple
-
-from PIL import Image, ImageDraw, ImageFont
-# from lib.waveshare_epd import epd7in5
+from PIL import Image, ImageDraw, ImageFont  # type: ignore
+# from lib.waveshare_epd import epd7in5 # type: ignore
 
 # XXX Set to desired system font or font from ./pic/
 # Recommended to use monospace font to ensure alignment shape and text
 # alignment
 # FreeMonoBold18: 11x18 pixels per char
-font18 = ImageFont.truetype("/usr/share/fonts/gnu-free/FreeMonoBold.otf", 18)
 # FreeMonoBold24: 15x24 pixels per char
-font24 = ImageFont.truetype("/usr/share/fonts/gnu-free/FreeMonoBold.otf", 24)
 
 WIDTH = 640
 HEIGHT = 384
+
+RADIUS = 10
 
 WEEKDAYS = {
     0: "Mon",
@@ -35,25 +27,11 @@ WEEKDAYS = {
     6: "Sun"
 }
 
-# draw = ImageDraw.Draw(img)
-
-# draw.text((20, 5), WEEKDAYS[today.weekday()], font=font18, fill=0)
-# draw.ellipse([(20, 24), (53, 57)], fill=0, outline=0)
-# draw.text((21.5, 28.5), f'{today.day}', font=font24, fill=255)
-
-# TODO comment out when running on pi
-# img.show()
-
-# TODO uncomment for actual display
-# epd.display(epd.getbuffer(img))
-# time.sleep(2)
-# epd.Clear()
-# time.sleep(2)
-# epd.sleep()
 
 class Draw:
     '''Draw class to simplify displaying calendar information'''
-    def __init__(self):
+
+    def __init__(self) -> None:
         # self.epd = epd7in5.EPD()
         # self.epd.init()
         # self.epd.Clear()
@@ -66,30 +44,37 @@ class Draw:
         self.font24 = ImageFont.truetype(
             "/usr/share/fonts/opentype/freefont/FreeMonoBold.otf", 24)
         self.draw_screen = ImageDraw.Draw(self.screen)
+        # event: title, time, location, privacy
+        self.events: List[Dict[str, str]] = [{
+            'title': 'event1',
+            'time': '2019-11-16 17:00:00',
+            'location': '...here',
+            'privacy': 'none'
+        }]
+        self.days: Dict[int, float] = {}
+        self.times: Dict[Tuple[int, int], float] = {}
 
     def show_away(self) -> None:
         '''Setup display to show away screen'''
 
     def show_calendar(self) -> None:
         '''Setup display to show calendar'''
-        # setup date in upper left corner
-        # self.draw_screen.text((70, 5), WEEKDAYS[today.weekday()],
-        #                       font=self.font18, fill=0)
-        # self.draw_screen.ellipse([(70, 24), (103, 57)], fill=0, outline=0)
-        # self.draw_screen.text((71.5, 28.5), f'{today.day}', font=self.font24,
-        #                       fill=255)
-
         today = datetime.today()
+
         # setup time grid for day
+        # Get width for 3 days to be displayed
         date_line: float = 65
         grid_width: float = (WIDTH - 10) - date_line
         day_width: float = grid_width / 3
 
+        # Get spacing to display 5 time lines
         time_line: float = 60
         grid_height: float = (HEIGHT - 10) - time_line
         time_height: float = grid_height / 5
 
+        # Draw lines for each day displayed
         for i in range(3):
+            self.days[today.day + i] = date_line
             self.draw_screen.line([(date_line, 60),
                                    (date_line, (HEIGHT - 10))], fill=0)
             self.draw_screen.text(((date_line + 5), 5),
@@ -102,14 +87,16 @@ class Draw:
             self.draw_screen.text(((date_line + 6.5), 28.5),
                                   f'{today.day + i}',
                                   font=self.font24, fill=255)
-
             date_line += day_width
 
+        # Draw last line to close calendar grid
         self.draw_screen.line([(date_line, 60),
                                (date_line, (HEIGHT - 10))], fill=0)
 
+        # Draw time lines
         current_hour, current_minute = get_initial_time(today)
         while time_line < HEIGHT:
+            self.times[(current_hour, current_minute)] = time_line
             self.draw_screen.line([(60, time_line),
                                    ((WIDTH - 10), time_line)], fill=0)
             self.draw_screen.text((6, (time_line - 9)),
@@ -122,6 +109,61 @@ class Draw:
 
             time_line += time_height
 
+        for event in self.events:
+            event_time = datetime.strptime(
+                event['time'], "%Y-%m-%d %H:%M:%S")
+
+            # TODO these need to be found to the nearest half hour then adjusted
+            # when they're in between because only the lines drawn on half hour
+            # intervals
+            # TODO only +/- 2 pixels from top/bottom when time falls on time
+            # line boundary
+            # TODO add case when an event beginning has already already passed
+            # but the event hasn't ended yet
+            etop = self.times[(event_time.hour, event_time.minute)] + 2
+            eleft = self.days[event_time.day] + 2
+            eright = self.days[event_time.day + 1] - 2
+            ebottom = self.times[((event_time.hour + 1),
+                                  event_time.minute)] - 2
+
+            # draw side lines with space to create rounded corners
+            # top
+            self.draw_screen.line(
+                [((eleft + RADIUS), etop), ((eright - RADIUS), etop)],
+                fill=0, width=1)
+            # bottom
+            self.draw_screen.line(
+                [((eleft + RADIUS), ebottom), ((eright - RADIUS), ebottom)],
+                fill=0, width=1)
+            # left
+            self.draw_screen.line(
+                [(eleft, (etop + RADIUS)), (eleft, (ebottom - RADIUS))],
+                fill=0, width=1)
+            # right
+            self.draw_screen.line(
+                [(eright, (etop + RADIUS)), (eright, (ebottom - RADIUS))],
+                fill=0, width=1)
+
+            # draw arcs to connect event sides
+            # top left corner
+            self.draw_screen.arc(
+                [(eleft, etop), ((eleft + (2*RADIUS)), (etop + (2*RADIUS)))],
+                180, 270, fill=0, width=1)
+            # top right corner
+            self.draw_screen.arc(
+                [((eright - (2*RADIUS)), etop), (eright, (etop + (2*RADIUS)))],
+                270, 0, fill=0, width=1)
+            # bottom left corner
+            self.draw_screen.arc(
+                [(eleft, (ebottom - (2*RADIUS))),
+                 ((eleft + (2*RADIUS)), ebottom)],
+                90, 180, fill=0, width=1)
+            # bottom right corner
+            self.draw_screen.arc(
+                [((eright - (2*RADIUS)), (ebottom - (2*RADIUS))),
+                 (eright, ebottom)],
+                0, 90, fill=0, width=1)
+
     def display(self) -> None:
         '''Send image to display'''
         # TODO comment out when running on pi
@@ -132,17 +174,19 @@ class Draw:
         # time.sleep(2)
         # self.epd.sleep()
 
-    def add_event(self, event) -> None:
+    def add_event(self, events: List[Dict[str, str]]) -> None:
         '''Add event to calendar
         event: to be added to screen
         '''
         # TODO figure out how events will be structured
-        # even = dict:
+        # event = dict:
         #       title
         #       time
         #       name
         #       location
         #       privacy
+
+        self.events += events
 
 
 def get_initial_time(today: datetime) -> Tuple[int, int]:
